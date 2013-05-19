21d9dcd096828a8793bc004788c06649d3749487
Anand Pant <theapant@gmail.com>
2013-05-19 14:02:41 -0500

    initial cos

--- cos.mk
+++ cos.mk
@@ -1,8 +1,8 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cos/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common cos stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
@@ -10,7 +10,7 @@ $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
 PRODUCT_RELEASE_NAME := ancora_tmo
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora_tmo
+PRODUCT_NAME := cos_ancora_tmo
 PRODUCT_DEVICE := ancora_tmo
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_ancora_tmo-userdebug
+add_lunch_combo cos_ancora_tmo-userdebug
